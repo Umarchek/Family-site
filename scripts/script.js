9fed3a40af23020cab2d4c1831861928cb28dc40
UZBase <sharofiddinov.07@mail.ru>
2021-12-29 12:58:02 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 window.addEventListener('load', function (e) {
-    // body
+    const changeMap = document.querySelector(".section-big-maps-buttons")
+    const map = document.querySelector('.map')
 
+
+    changeMap.addEventListener('click', function () {
+        toggle()
+    })
+    function toggle(){
+        if(map.classList.contains('ChangedMap')){
+            map.classList.remove("ChangedMap")
+        }else{
+            map.classList.add("ChangedMap")
+        }
+    }
 });
