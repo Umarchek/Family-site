59d1f4845d762874ed7a8e28d4e32e0dfe23debc
UZBase <sharofiddinov.07@mail.ru>
2021-12-29 14:01:50 +0300

    end

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
 window.addEventListener('load', function (e) {
     const changeMap = document.querySelector(".section-big-maps-buttons")
     const map = document.querySelector('.map')
-
+    const logo = document.querySelector('.section-big-maps-logo')
 
     changeMap.addEventListener('click', function () {
         toggle()
     })
-    function toggle(){
-        if(map.classList.contains('ChangedMap')){
-            map.classList.remove("ChangedMap")
-        }else{
-            map.classList.add("ChangedMap")
-        }
+    function toggle() {
+        map.classList.add("ChangedMap")
+        logo.style.display = 'block';
+        changeMap.style.display = 'none';
     }
+
+    logo.addEventListener('click', function () {
+        doggle()
+    })
+    function doggle() {
+        map.classList.remove("ChangedMap")
+        changeMap.style.display = 'block';
+        logo.style.display = 'none';
+    }
+
 });
